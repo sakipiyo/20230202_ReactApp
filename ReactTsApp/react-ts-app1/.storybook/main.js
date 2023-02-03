b676c8e55625b109c566ea1684d7ffe0c7f0acdb
Yuto Kawasaki <chyole4444bakaparm@msn.com>
2023-02-03 19:00:20 +0900

    Setting Storybook

--- ReactTsApp/react-ts-app1/.storybook/main.js
+++ ReactTsApp/react-ts-app1/.storybook/main.js
@@ -1,3 +1,6 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,10 +9,25 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-postcss",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+  staticDirs: ['public'],
+  babel: async options => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  // 必要なアドオンを導入し、tsconfigの設定を引き継ぐため
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json')
+      }),
+    ];
+
+    return config
+  },
 }
